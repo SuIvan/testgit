82f8a6a82ee353e3f2ab7db746fb6bf3225f3868
ivan <SYF2017i>
2018-10-15 14:59:49 +0800

    upload a new file to repository

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @author : ivan
+ * @version : v1.00
+ * @CreationDate : 2018/10/15 14:57
+ * @Description :
+ * @update : 修改人，修改时间，修改内容
+ * @see ：[相关类/方法]
+ */
+public class Test {
+    private String id;
+    private String name;
+
+}
